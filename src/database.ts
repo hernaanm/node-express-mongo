b2d9e5801fd23c8f58bf59f70a161f453dc2f6c0
Hernan Mateika <hernan.mateika@aircall.io>
2021-09-17 03:17:07 +0200

    changing package manager from npm to yarn

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export default async function connect(){
-try {
-   await  mongoose.connect('mongodb://localhost/ts-app', {
-        useNewUrlParser : true,
-        useUnifiedTopology: true
-    })
-    console.log('>>Database connected<<')    
-} catch (error) {
-    console.log('Cannot connect to DB ')
-}
-
+export default async function connect() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/ts-app", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(">>Database connected<<");
+  } catch (error) {
+    console.log("Cannot connect to DB ");
+  }
 }
